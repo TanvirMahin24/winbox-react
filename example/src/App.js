72412973dacfcc7b9a9d6b3d033b70dc65d512f4
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-05-15 20:24:18 +0600

    basic winbox react done

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,54 @@
 import React from 'react'
 
-import { ExampleComponent } from 'winbox-react'
+import WinboxReact from 'winbox-react'
 import 'winbox-react/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent />
+  const options = {
+    // ADDING CUSTOM TITLE
+    title: 'Custom Title',
+
+    //ADDING BORDER example:  25px, 1rem, 10%, 3em. If any number is given it will be count as pixels
+    border: '4',
+
+    //ADDING BACKGROUND COLOR any valid css color code can be used. You also can use gradients.
+    background:
+      'linear-gradient(90deg, rgba(49,36,239,1) 0%, rgba(67,0,168,1) 100%)',
+
+    //CUSTOM className
+    className: '',
+
+    //CUSTOM POSITION any valid
+    x: 'center',
+    y: 'center',
+
+    //CUSTOM SIZE
+    width: '20%',
+    height: '50%',
+
+    //LIMIT VIEWPORT
+    top: 50,
+    right: 50,
+    bottom: 100,
+    left: 50,
+
+    //IS MODAL OR WINDOW (default is false)
+    modal: false,
+
+    //EMBED A WEBSITE using the url prop ()
+    url: '',
+
+    //USE THE MODERN THEME OF WINBOX
+    modern: false
+  }
+  return (
+    <WinboxReact {...options}>
+      <h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+        inventore ut fugit iusto ex ad nihil, fuga ipsum tenetur itaque?
+      </h4>
+    </WinboxReact>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
 import React from 'react'
+import ReactDOMServer from 'react-dom/server'
 import WinBox from 'winbox/src/js/winbox'
 import 'winbox/dist/css/winbox.min.css'
-import styles from './styles.module.css'
+import 'winbox/dist/css/themes/modern.min.css'
 
-export const ExampleComponent = ({}) => {
-  const modal = new WinBox()
-  modal.setBackground('var(--color_orange)')
-  modal.setTitle(`${institution}`)
-  return <div className={styles.test}>Example Component: </div>
+const WinboxReact = ({
+  title,
+  border,
+  background,
+  className,
+  x,
+  y,
+  width,
+  height,
+  top,
+  right,
+  bottom,
+  left,
+  modal,
+  children,
+  modern
+}) => {
+  const wb = new WinBox(title, {
+    border,
+    background,
+    x,
+    y,
+    width,
+    height,
+    top,
+    right,
+    bottom,
+    left,
+    modal,
+    html: `${ReactDOMServer.renderToStaticMarkup(children)}`,
+    class: `${modern ? 'modern' : className ? `${className}` : ''}`
+  })
+
+  return <div className={className ? `${$className}` : ''}></div>
 }
+
+WinboxReact.defaultProps = {
+  title: 'Winbox React Title',
+  border: '0',
+  background: '#28292d',
+  x: 0,
+  y: 0,
+  modal: false
+}
+
+export default WinboxReact
