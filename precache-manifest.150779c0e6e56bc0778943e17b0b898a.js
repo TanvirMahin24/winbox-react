4f3124853124806b21ae641e162d35080a5ed692
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-05-15 23:29:29 +0600

    Updates

---
+++ precache-manifest.150779c0e6e56bc0778943e17b0b898a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6ba29ad08272ae2cf02218be826202",
+    "url": "./index.html"
+  },
+  {
+    "revision": "afeac2bd25cfdf633a94",
+    "url": "./static/css/2.615f6ff3.chunk.css"
+  },
+  {
+    "revision": "2ca259c859d80552e0d1",
+    "url": "./static/css/main.7abb72a0.chunk.css"
+  },
+  {
+    "revision": "afeac2bd25cfdf633a94",
+    "url": "./static/js/2.872bfff0.chunk.js"
+  },
+  {
+    "revision": "45416e442b016433136cece769172fb2",
+    "url": "./static/js/2.872bfff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca259c859d80552e0d1",
+    "url": "./static/js/main.14847ed4.chunk.js"
+  },
+  {
+    "revision": "da6e1857291de4855bb602d67c39a264",
+    "url": "./static/js/main.14847ed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a65de50c790c500016e8",
+    "url": "./static/js/runtime-main.56ef061e.js"
+  }
+]);
